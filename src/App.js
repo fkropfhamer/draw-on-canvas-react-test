9c6c0d7a77e0f87ba6d309d231dfefd61166fcf5
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-11-07 11:37:03 +0100

    updated example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {useRef} from 'react';
 import DrawOnCanvas from 'draw-on-canvas-react'
 
 function App() {
+  const ref = {}
+
   return (
     <div className="App">
-      <DrawOnCanvas />
+      <DrawOnCanvas width={300} height={200} strokeColor={'green'} strokeWeight={20} drawReference={ref}/>
+      <button onClick={() => {ref.current.reset()}}>Reset</button>
+      <button onClick={() => {}}>GET</button>
     </div>
   );
 }
