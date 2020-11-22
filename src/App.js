6b771686ee98ac40512d53be2c38f3205cc1a7d6
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-11-22 16:10:02 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import {useRef} from 'react';
+import {useRef, useState} from 'react';
 import DrawOnCanvas from 'draw-on-canvas-react'
 
 function App() {
-  const ref = {}
+  const ref = useRef()
 
   return (
     <div className="App">
-      <DrawOnCanvas width={300} height={200} strokeColor={'green'} strokeWeight={20} drawReference={ref}/>
-      <button onClick={() => {ref.current.reset()}}>Reset</button>
-      <button onClick={() => {}}>GET</button>
+      <DrawOnCanvas width={300} height={200} strokeColor={'green'} strokeWeight={20} ref={ref}/>
+      <button onClick={() => ref.current.draw.reset()}>Reset</button>
+      <button onClick={() => console.log(ref.current.draw.getPixelArray())}>GET</button>
+      <button onClick={() => ref.current.draw.changeStrokeColor('red')}>change Color</button>
     </div>
   );
 }
