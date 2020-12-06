462bc2fabbd148aaf30654b71f918bc2dcc7f621
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-12-06 17:50:15 +0100

    update

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ function App() {
   return (
     <div className="App">
       <DrawOnCanvas width={300} height={200} strokeColor={'green'} strokeWeight={20} ref={ref}/>
-      <button onClick={() => ref.current.draw.reset()}>Reset</button>
+      <button onClick={() => ref.current.reset()}>Reset</button>
       <button onClick={() => console.log(ref.current.draw.getPixelArray())}>GET</button>
-      <button onClick={() => ref.current.draw.changeStrokeColor('red')}>change Color</button>
+      <button onClick={() => ref.current.changeStrokeColor('red')}>change Color</button>
+      <button onClick={() => ref.current.downloadPNG()}>download</button>
     </div>
   );
 }
